5513536c521623371bdee8a1796c0aa7ed5d2b49
Jonathan Raoult <jesuisjonathan@gmx.fr>
2013-02-25 02:17:54 +1100

    #3 Youtube video search - added search pane

--- main/resources/script/controllers.js
+++ main/resources/script/controllers.js
@@ -1,26 +1,28 @@
 (function (mt) {
+    var staticVideosInstances = [
+//        {"id": "35FbLhYG86M", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/35FbLhYG86M/default.jpg", "duration": 340000},
+        // fake duration to make it easier to debug
+        {"id": "35FbLhYG86M", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/35FbLhYG86M/default.jpg", "duration": 10000},
+        // erroneous video ids used to test ping video
+//            {"id": "ypU6RHVb_G", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/ypU6RHVb_Gw/default.jpg", "duration": 255000},
+//            {"id": "YiC5SeRfLY", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/YiC5SeRfLYw/default.jpg", "duration": 282000},
+        {"id": "ypU6RHVb_Gw", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/ypU6RHVb_Gw/default.jpg", "duration": 255000},
+        {"id": "YiC5SeRfLYw", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/YiC5SeRfLYw/default.jpg", "duration": 282000},
+        {"id": "-B8IKn-RrDc", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/-B8IKn-RrDc/default.jpg", "duration": 402000},
+        {"id": "oBbHo8b4FDc", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/oBbHo8b4FDc/default.jpg", "duration": 270000},
+        {"id": "wvLv_Pem0BA", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/wvLv_Pem0BA/default.jpg", "duration": 268000},
+        {"id": "hhRWM-K5TD8", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/hhRWM-K5TD8/default.jpg", "duration": 258000},
+        {"id": "NNrporSoQf8", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/NNrporSoQf8/default.jpg", "duration": 261000},
+        {"id": "e3xXCfXxr70", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/e3xXCfXxr70/default.jpg", "duration": 192000},
+        {"id": "XPhUUAjSKD8", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/XPhUUAjSKD8/default.jpg", "duration": 288000}
+    ];
+
 // todo rename timeline to playlist
     mt.MixTubeApp.controller('mtTimelineCtrl', function ($scope, $rootScope, $q, mtYoutubeClient, mtLogger) {
 
         $scope.currentVideoInstanceIdx = 0;
 
-        $scope.videoInstances = [
-//        {"id": "35FbLhYG86M", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/35FbLhYG86M/default.jpg", "duration": 340000},
-            // fake duration to make it easier to debug
-            {"id": "35FbLhYG86M", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/35FbLhYG86M/default.jpg", "duration": 10000},
-            // erroneous video ids used to test ping video
-//            {"id": "ypU6RHVb_G", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/ypU6RHVb_Gw/default.jpg", "duration": 255000},
-//            {"id": "YiC5SeRfLY", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/YiC5SeRfLYw/default.jpg", "duration": 282000},
-            {"id": "ypU6RHVb_Gw", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/ypU6RHVb_Gw/default.jpg", "duration": 255000},
-            {"id": "YiC5SeRfLYw", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/YiC5SeRfLYw/default.jpg", "duration": 282000},
-            {"id": "-B8IKn-RrDc", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/-B8IKn-RrDc/default.jpg", "duration": 402000},
-            {"id": "oBbHo8b4FDc", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/oBbHo8b4FDc/default.jpg", "duration": 270000},
-            {"id": "wvLv_Pem0BA", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/wvLv_Pem0BA/default.jpg", "duration": 268000},
-            {"id": "hhRWM-K5TD8", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/hhRWM-K5TD8/default.jpg", "duration": 258000},
-            {"id": "NNrporSoQf8", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/NNrporSoQf8/default.jpg", "duration": 261000},
-            {"id": "e3xXCfXxr70", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/e3xXCfXxr70/default.jpg", "duration": 192000},
-            {"id": "XPhUUAjSKD8", "provider": "youtube", "thumbnailUrl": "https://i.ytimg.com/vi/XPhUUAjSKD8/default.jpg", "duration": 288000}
-        ];
+        $scope.videoInstances = staticVideosInstances;
 
         $scope.$on(mt.events.NextVideoInstanceRequest, function () {
             mtLogger.debug('Next video instance request received');
@@ -145,5 +147,57 @@
         $scope.requestFullscreen = function () {
             document.getElementById('mt-video-window').webkitRequestFullscreen();
         }
+    });
+
+    mt.MixTubeApp.controller('mtSearchCtrl', function ($scope, $rootScope, $timeout, mtYoutubeClient, mtLogger) {
+
+        /**
+         * @const
+         * @type {number}
+         */
+        var INSTANT_SEARCH_DELAY = 300;
+
+        /** @type {string} */
+        $scope.searchTerm = undefined;
+        /** @type {Array.<mt.model.Video>} */
+        $scope.youtubeSearchResults = staticVideosInstances;
+        /** @type {boolean} */
+        $scope.searchVisible = true;
+        /** @type {Promise} */
+        $scope.instantSearchPromise = undefined;
+
+        // when the user types we automatically execute the search
+        $scope.$watch('searchTerm', function (newSearchTerm) {
+            // new inputs so we stop the previous request
+            $timeout.cancel($scope.instantSearchPromise);
+
+            // if the search has to be longer than two characters
+            if (newSearchTerm && newSearchTerm.length > 2) {
+                $timeout.cancel($scope.instantSearchPromise);
+                $scope.instantSearchPromise = $timeout(function () {
+                    $scope.search();
+                }, INSTANT_SEARCH_DELAY);
+            }
+        });
+
+        $scope.search = function () {
+            mtYoutubeClient.searchVideosByQuery($scope.searchTerm).then(function (videos) {
+                $scope.youtubeSearchResults = videos;
+            });
+        };
+
+        /**
+         * Opens the search frame and optionally input the first char.
+         *
+         * @param {string=} firstChar the first char to fill the input with
+         */
+        $scope.open = function (firstChar) {
+            $scope.searchVisible = true;
+            $scope.searchTerm = firstChar;
+        };
+
+        $scope.close = function () {
+            $scope.searchVisible = false;
+        };
     });
 })(mt);
