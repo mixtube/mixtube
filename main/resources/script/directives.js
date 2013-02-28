5a21cbd235be8829b38465d1e82c346d7c27a5d9
Jonathan Raoult <jesuisjonathan@gmx.fr>
2013-03-01 02:05:59 +1100

    some progress on youtube search

--- main/resources/script/directives.js
+++ main/resources/script/directives.js
@@ -1,17 +1,18 @@
 (function (mt) {
     // a directive to link focus state of a field with model expression
-    mt.MixTubeApp.directive('mtFocus', function ($parse, $timeout) {
+    var FOCUS_DIRECTIVE_NAME = 'mtFocus';
+    mt.MixTubeApp.directive(FOCUS_DIRECTIVE_NAME, function ($parse, $timeout) {
         return {
             restrict: 'A',
             link: function (scope, elmt, attrs) {
-                var setter = $parse(attrs.mtFocus).assign;
-                if (setter) {
-                    angular.element(elmt[0]).bind('blur', function () {
-                        setter(scope, false);
+                var fn = $parse(attrs[FOCUS_DIRECTIVE_NAME]);
+                elmt.bind('blur', function () {
+                    scope.$apply(function () {
+                        fn.assign(scope, false);
                     });
-                }
+                });
 
-                scope.$watch(attrs.mtFocus, function (value) {
+                scope.$watch(attrs[FOCUS_DIRECTIVE_NAME], function (value) {
                     if (value === true) {
                         $timeout(function () {
                             elmt[0].focus();
@@ -20,5 +21,22 @@
                 });
             }
         };
+    });
+
+    // a duration formatter that takes a duration in milliseconds and returns a formatted duration like "h:mm"
+    mt.MixTubeApp.filter('mtDuration', function () {
+        // time that represent the absolute zero for date, all fields to zero
+        var ABSOLUTE_DATE_ZERO = -2209114800000;
+
+        // reuse the date object between invocation since it is only used as a formatting tool
+        var singletonDate = new Date();
+
+        return function (time) {
+            // reset the time to the zero to calculate durations
+            singletonDate.setTime(ABSOLUTE_DATE_ZERO);
+            singletonDate.setMilliseconds(time);
+
+            return (singletonDate.getHours() * 60 + singletonDate.getMinutes()) + ':' + singletonDate.getSeconds();
+        }
     });
 })(mt);
