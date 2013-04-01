8b4f6be959d842d3433cae9f3c35d6b6eee10811
Jonathan Raoult <jesuisjonathan@gmx.fr>
2013-04-01 23:28:47 +1100

    fixed two duration formatting problems (left padding function) and added documentation

--- main/resources/script/bootstrap.js
+++ main/resources/script/bootstrap.js
@@ -58,16 +58,45 @@
     };
 
     mt.tools = {__uniqueIdCounter: 0};
-    mt.tools.findWhere = function (array, attrs) {
+
+    /**
+     * Finds the first elements in a array where the properties match the ones given.
+     *
+     * @param {Array} array the arrays to look into
+     * @param {Object} properties the filter
+     * @return {?Object} the found element or null
+     */
+    mt.tools.findWhere = function (array, properties) {
         var result = array.filter(function (value) {
-            for (var key in attrs) {
-                if (attrs[key] !== value[key]) return false;
+            for (var key in properties) {
+                if (properties[key] !== value[key]) return false;
             }
             return true;
         });
         return result.length > 0 ? result[0] : null;
     };
+    /**
+     * Generates a "unique" id.
+     *
+     * @return {string}
+     */
     mt.tools.uniqueId = function () {
         return 'mt_uid_' + mt.tools.__uniqueIdCounter++;
+    };
+    /**
+     * Pads a string to the left.
+     *
+     * @param string the string to pad
+     * @param length the expected length after padding
+     * @param padString the string to pad with
+     * @return {string}
+     */
+    mt.tools.leftPad = function (string, length, padString) {
+        if (!angular.isString(string)) throw new Error('The string parameter should be a string');
+
+        while (string.length < length) {
+            string = padString + string;
+        }
+        return string;
     };
 })(window.mt = window.mt || {});
