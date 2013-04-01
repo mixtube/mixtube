b6c287e668dc6d9f1d05729e28c8954533d6aec9
Jonathan Raoult <jesuisjonathan@gmx.fr>
2013-04-01 23:27:32 +1100

    fixed two duration formatting problems : - padded of one digit seconds with 0 - fixed the way to reset a date object that causes trouble after summer/winter time shift

--- main/resources/script/directives-filters.js
+++ main/resources/script/directives-filters.js
@@ -25,22 +25,22 @@
 
     // a duration formatter that takes a duration in milliseconds and returns a formatted duration like "h:mm"
     mt.MixTubeApp.filter('mtDuration', function () {
-        // time that represent the absolute zero for date, all fields to zero
-        var ABSOLUTE_DATE_ZERO = -2209114800000;
-
         // reuse the date object between invocation since it is only used as a formatting tool
-        var singletonDate = new Date();
+        var singletonDate = new Date(0, 0, 0, 0, 0, 0, 0);
+        // time that represent the absolute zero for date, all fields to zero
+        // needs to be computed because of timezones differences
+        var absoluteDateZero = singletonDate.getTime();
 
         return function (time) {
-            if(isNaN(time)) {
+            if (isNaN(time)) {
                 return '';
             }
 
             // reset the time to the zero to calculate durations
-            singletonDate.setTime(ABSOLUTE_DATE_ZERO);
+            singletonDate.setTime(absoluteDateZero);
             singletonDate.setMilliseconds(time);
 
-            return (singletonDate.getHours() * 60 + singletonDate.getMinutes()) + ':' + singletonDate.getSeconds();
+            return (singletonDate.getHours() * 60 + singletonDate.getMinutes()).toString(10) + ':' + mt.tools.leftPad(singletonDate.getSeconds().toString(10), 2, '0');
         }
     });
 })(mt);
