552363fa28541334f25573c8fda4ca4a84312039
Jonathan Raoult <jesuisjonathan@gmx.fr>
2014-03-10 18:28:01 +0100

    updated orchestrator logic

--- app/scripts/v2/components/playback/orchestrator.js
+++ app/scripts/v2/components/playback/orchestrator.js
@@ -3,34 +3,106 @@
 
     mt.MixTubeApp.factory('mtOrchestrator', function ($q, mtQueueManager, mtMediaElementsPool) {
 
-        var runningPopcorns = [];
+        var _pendingPlayer = null;
+        var _runningPlayers = [];
+
+        function freePlayer(player) {
+            player.popcorn.destroy();
+            player.wrapper.release();
+        }
+
+        /**
+         * Fades out, free and remove the currently running players from the list
+         */
+        function initiateRunningPlayersTermination() {
+            _runningPlayers.forEach(function (runningPlayer) {
+                runningPlayer.popcorn.fade({direction: 'out', duration: 5, done: function () {
+                    freePlayer(runningPlayer);
+                    _.remove(_runningPlayers, runningPlayer);
+                }});
+            });
+        }
+
+        function preparePending(queueEntry) {
+            var deferred = $q.defer();
+
+            var wrapper = mtMediaElementsPool(queueEntry.video.provider);
+            var popcorn = Popcorn(wrapper.get());
+
+            // if there is already a pending player we want to cancel: most recent request always wins
+            if (_pendingPlayer) {
+                freePlayer(_pendingPlayer);
+            }
+            _pendingPlayer = {wrapper: wrapper, popcorn: popcorn};
+
+            popcorn.on('canplay', function preparePendingCanPlayCb() {
+                popcorn.off('canplay', preparePendingCanPlayCb);
+                deferred.resolve();
+            });
+
+            wrapper.get().src = 'http://www.youtube.com/watch?v=' + queueEntry.video.id;
+
+            return deferred.promise;
+        }
+
+        /**
+         * @returns {promise}
+         */
+        function startPending() {
+            var deferred = $q.defer();
+            var popcorn = _pendingPlayer.popcorn;
+
+            if (popcorn.readyState < HTMLMediaElement.HAVE_FUTURE_DATA) {
+                // safety checking: there is a race condition here and it shouldn't happen
+                deferred.reject(new Error('The pending popcorn instance is not ready to be play started'));
+            } else {
+                // register the cue to initiate the transition before the end (auto cross-fade)
+                popcorn.cue(popcorn.duration() - 5, function () {
+
+                });
+                popcorn.play();
+                popcorn.fade({direction: 'in', duration: 5});
+                initiateRunningPlayersTermination();
+                // promote the pending player to the running players list
+                _runningPlayers.push(_pendingPlayer);
+                _pendingPlayer = null;
+                deferred.resolve();
+            }
+
+            return deferred.promise;
+        }
 
         return {
             skipTo: function (queueEntry) {
 
-                var playingDeferred = $q.defer();
-
-                var wrapper = mtMediaElementsPool(queueEntry.video.provider);
-                var popcorn = Popcorn(wrapper.get());
-
-                popcorn.on('loadedmetadata', function () {
-                    popcorn.fade({direction: 'in', duration: 5});
-
-                    runningPopcorns.forEach(function (runningPopcorns) {
-                        runningPopcorns.fade({direction: 'out', duration: 5});
-                    });
-
-                    runningPopcorns = [popcorn];
+                return preparePending(queueEntry).then(function () {
+                    return startPending();
                 });
 
-                popcorn.on('canplay', function () {
-                    popcorn.play();
-                    playingDeferred.resolve();
-                });
-
-                wrapper.get().src = 'http://www.youtube.com/watch?v=' + queueEntry.video.id;
-
-                return playingDeferred.promise;
+//                var playingDeferred = $q.defer();
+//
+//                var wrapper = mtMediaElementsPool(queueEntry.video.provider);
+//                var popcorn = Popcorn(wrapper.get());
+//
+//                if (_pendingPlayer) {
+//                    freePlayer(_pendingPlayer);
+//                }
+//                _pendingPlayer = {wrapper: wrapper, popcorn: popcorn};
+//
+//                popcorn.on('loadedmetadata', function () {
+//                    popcorn.cue(popcorn.duration() - 5, function () {
+//                        play(_pendingPlayer.popcorn);
+//                    });
+//                });
+//
+//                popcorn.on('canplay', function () {
+//                    play(_pendingPlayer.popcorn);
+//                    playingDeferred.resolve();
+//                });
+//
+//                wrapper.get().src = 'http://www.youtube.com/watch?v=' + queueEntry.video.id;
+//
+//                return playingDeferred.promise;
             }
         };
     });
--- app/scripts/v2/components/playback/popcorn.fade.js
+++ app/scripts/v2/components/playback/popcorn.fade.js
@@ -2,20 +2,45 @@
 
     'use strict';
 
-    var volumeTweenable = new Tweenable();
-    var opacityTweenable = new Tweenable();
+    function CountDownLatch(count, done) {
+        this.count = count;
+        this.done = done;
+    }
+
+    CountDownLatch.prototype = {
+        countDown: function () {
+            var latch = this;
+            if (!--latch.count) {
+                window.setTimeout(function () {
+                    latch.done();
+                }, 0);
+            }
+        }
+    };
 
     function volumeStep(popcorn, state) {
         popcorn.volume(state.volume);
-        console.log(popcorn.volume());
     }
 
     function opacityStep(popcorn, state) {
         popcorn.media.style.opacity = state.opacity;
     }
 
+    function stopTweenable(tweenable) {
+        if (tweenable && tweenable.isPlaying()) {
+            tweenable.stop();
+        }
+    }
+
     Popcorn.prototype.fade = function (options) {
         var instance = this;
+
+        // if there is a running fade action stop it and start the new one
+        stopTweenable(instance.volumeTweenable);
+        instance.volumeTweenable = new Tweenable();
+
+        stopTweenable(instance.opacityTweenable);
+        instance.opacityTweenable = new Tweenable();
 
         var durationInMillis = options.duration * 1000;
 
@@ -39,7 +64,15 @@
             volumeTweenableConfig.to.volume = opacityTweenableConfig.to.opacity = 0;
         }
 
-        volumeTweenable.tween(volumeTweenableConfig);
-        opacityTweenable.tween(opacityTweenableConfig);
+        //  we instantiate the latch only if the caller is interested in knowing when the fade is finished
+        if ('done' in options) {
+            var latch = new CountDownLatch(2, options.done);
+            volumeTweenableConfig.finish = opacityTweenableConfig.finish = function () {
+                latch.countDown();
+            };
+        }
+
+        instance.volumeTweenable.tween(volumeTweenableConfig);
+        instance.opacityTweenable.tween(opacityTweenableConfig);
     };
 })(Popcorn);
