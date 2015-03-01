bce6ad6bd6a3f605eb6ef6ba30e2bee78f614bed
Jonathan Raoult <jesuisjonathan@gmx.fr>
2015-03-02 00:01:01 +1100

    #46 Rationalise build scripts - got rid of grunt - got rid of ruby sass

--- Gemfile
+++
@@ -1,3 +0,0 @@
-source 'https://rubygems.org'
-gem 'sass', '~>3.4.6'
-gem 'bourbon', '~> 4.0.2'
--- gulpfile.js
+++ gulpfile.js
@@ -3,38 +3,69 @@
 var path = require('path'),
   gulp = require('gulp'),
   gutil = require('gulp-util'),
+  buffer = require('vinyl-buffer'),
   source = require('vinyl-source-stream'),
   watchify = require('watchify'),
-  browserify = require('browserify');
+  browserify = require('browserify'),
+  sass = require('gulp-sass'),
+  please = require('gulp-pleeease'),
+  sourcemaps = require('gulp-sourcemaps'),
+  browserSync = require('browser-sync'),
+  reload = browserSync.reload;
 
-function installWatchify(src, dest) {
-  var bundler = watchify(
-    browserify(src,
-      {
-        cache: {},
-        packageCache: {},
-        fullPaths: true,
-        debug: true
-      }));
+function buildBundleJS(src, dest) {
+  var bundler = watchify(browserify(src, watchify.args));
+  bundler.on('log', gutil.log);
 
-  bundler.on('update', function() {
-    gutil.log('Bundle "' + src + '" updated');
-    rebundle();
-  });
-
-  function rebundle() {
+  function bundleJS() {
     return bundler.bundle()
+      // log errors if they happen
       .on('error', gutil.log.bind(gutil, 'Browserify Error'))
       .pipe(source(path.basename(dest)))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest(path.dirname(dest)));
   }
 
-  return rebundle();
+  bundleJS.bundler = bundler;
+  return bundleJS;
 }
 
-gulp.task('watch', function() {
+var bundleAppJS = buildBundleJS('./app/scripts/app.js', './build/scripts/app.bundle.js');
 
-  installWatchify(
-    './app/scripts/app.js',
-    './build/app/scripts/app.bundle.js');
+gulp.task('script', function() {
+  bundleAppJS();
+});
+
+gulp.task('style', function() {
+  return gulp.src('app/styles/css/main.scss', {base: 'app'})
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(please({
+      import: false,
+      minifier: false,
+      mqpacker: false,
+      autoprefixer: ['last 1 version'],
+      variables: false,
+      rem: false,
+      pseudoElements: false
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('watch', function() {
+  bundleAppJS.bundler.on('update', bundleAppJS);
+  gulp.watch('app/**/*.scss', ['style']);
+});
+
+gulp.task('serve', ['watch'], function() {
+  browserSync({
+    open: false,
+    server: {
+      baseDir: ['app', 'build']
+    }
+  });
 });
