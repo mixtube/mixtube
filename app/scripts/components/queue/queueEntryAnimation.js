524a705db52581945bb629b5a19554520f379479
Jonathan Raoult <jesuisjonathan@gmx.fr>
2015-09-16 00:38:02 +1000

    Removed VelocityJS dependency: - removed commented code

--- app/scripts/components/queue/queueEntryAnimation.js
+++ app/scripts/components/queue/queueEntryAnimation.js
@@ -47,56 +47,6 @@ function queueEntryAnimation(slideAnimationBuilder, foldAnimationBuilder) {
     enter: enter,
     leave: leave
   };
-
-
-
-  //return assign(
-  //  slideSizeAnimationBuilder(),
-  //  {
-  //    enter: function(element, done) {
-  //
-  //      var txBeginning = '-100%';
-  //      var scope = element.scope();
-  //
-  //      scope.$emit('mtQueueEntryAnimation::started', scope.entry);
-  //
-  //      // first step of the animation
-  //      element.css({transform: 'translateX(' + txBeginning + ')'});
-  //
-  //      // second step that may be delayed
-  //      function nextStep() {
-  //        velocity(
-  //          element[0],
-  //          {translateX: [0, txBeginning]},
-  //          defaults({
-  //            complete: function() {
-  //              element.css({transform: ''});
-  //              done();
-  //            }
-  //          }, slideSizeAnimationBuilder.BASE_VELOCITY_ANIM_CONF));
-  //      }
-  //
-  //      var suspended = false;
-  //      var continuation = {
-  //        suspend: function() {
-  //          suspended = true;
-  //        },
-  //        continue: function() {
-  //          suspended = false;
-  //          nextStep();
-  //        }
-  //      };
-  //
-  //      // may be overkill because it is triggered just after started but describes better the two phases
-  //      // nature of this animation
-  //      scope.$emit('mtQueueEntryAnimation::sizingDone', scope.entry, continuation);
-  //
-  //      // at this stage the continuation may have been suspended by someone listening to the event
-  //      if (!suspended) {
-  //        continuation.continue();
-  //      }
-  //    }
-  //  });
 }
 
 module.exports = queueEntryAnimation;
