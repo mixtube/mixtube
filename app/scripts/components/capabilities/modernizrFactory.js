4172f4b961d2e2305af3df8ab4ea318bded21774
Jonathan Raoult <jesuisjonathan@gmx.fr>
2015-09-20 18:55:09 +1000

    Task: updated Modernizr

--- app/scripts/components/capabilities/modernizrFactory.js
+++ app/scripts/components/capabilities/modernizrFactory.js
@@ -4,8 +4,8 @@ function modernizerFactory() {
   var local = {};
 
   /*!
-   * modernizr v3.0.0-alpha.3
-   * Build http://v3.modernizr.com/download/#-videoautoplay-dontmin
+   * modernizr v3.0.0-alpha.4
+   * Build http://modernizr.com/download/#-videoautoplay-dontmin
    *
    * Copyright (c)
    *  Faruk Ates
@@ -30,12 +30,48 @@ function modernizerFactory() {
 
   ;
   (function(window, document, undefined) {
+    var classes = [];
+
+
+    /**
+     * is returns a boolean if the typeof an obj is exactly type.
+     *
+     * @access private
+     * @function is
+     * @param {*} obj - A thing we want to check the type of
+     * @param {string} type - A string to compare the typeof against
+     * @returns {boolean}
+     */
+
+    function is(obj, type) {
+      return typeof obj === type;
+    }
+    ;
+
+    /**
+     * docElement is a convenience wrapper to grab the root element of the document
+     *
+     * @access private
+     * @returns {HTMLElement|SVGElement} The root element of the document
+     */
+
+    var docElement = document.documentElement;
+
+
     var tests = [];
 
 
+    /**
+     *
+     * ModernizrProto is the constructor for Modernizr
+     *
+     * @class
+     * @access public
+     */
+
     var ModernizrProto = {
       // The current version, dummy
-      _version: '3.0.0-alpha.3',
+      _version: '3.0.0-alpha.4',
 
       // Any settings that don't work as separate modules
       // can go in here as configuration.
@@ -53,10 +89,9 @@ function modernizerFactory() {
       on: function(test, cb) {
         // I don't really think people should do this, but we can
         // safe guard it a bit.
-        // -- NOTE:: this gets WAY overridden in src/addTest for
-        // actual async tests. This is in case people listen to
-        // synchronous tests. I would leave it out, but the code
-        // to *disallow* sync tests in the real version of this
+        // -- NOTE:: this gets WAY overridden in src/addTest for actual async tests.
+        // This is in case people listen to synchronous tests. I would leave it out,
+        // but the code to *disallow* sync tests in the real version of this
         // function is actually larger than this.
         var self = this;
         setTimeout(function() {
@@ -75,32 +110,23 @@ function modernizerFactory() {
 
 
 
-    // Fake some of Object.create
-    // so we can force non test results
-    // to be non "own" properties.
+    // Fake some of Object.create so we can force non test results to be non "own" properties.
     var Modernizr = function() {
     };
     Modernizr.prototype = ModernizrProto;
 
-    // Leak modernizr globally when you `require` it
-    // rather than force it here.
+    // Leak modernizr globally when you `require` it rather than force it here.
     // Overwrite name so constructor name is nicer :D
     Modernizr = new Modernizr();
 
 
 
-    var classes = [];
-
-
     /**
-     * is returns a boolean for if typeof obj is exactly type.
+     * Run through all tests and detect their support in the current UA.
+     *
+     * @access private
      */
-    function is(obj, type) {
-      return typeof obj === type;
-    }
-    ;
 
-    // Run through all tests and detect their support in the current UA.
     function testRunner() {
       var featureNames;
       var feature;
@@ -114,12 +140,12 @@ function modernizerFactory() {
         featureNames = [];
         feature = tests[featureIdx];
         // run the test, throw the return value into the Modernizr,
-        //   then based on that boolean, define an appropriate className
-        //   and push it into an array of classes we'll join later.
+        // then based on that boolean, define an appropriate className
+        // and push it into an array of classes we'll join later.
         //
-        //   If there is no name, it's an 'async' test that is run,
-        //   but not directly added to the object. That should
-        //   be done with a post-run addTest call.
+        // If there is no name, it's an 'async' test that is run,
+        // but not directly added to the object. That should
+        // be done with a post-run addTest call.
         if (feature.name) {
           featureNames.push(feature.name.toLowerCase());
 
@@ -165,19 +191,73 @@ function modernizerFactory() {
 
     ;
 
-    var docElement = document.documentElement;
+    /**
+     * hasOwnProp is a shim for hasOwnProperty that is needed for Safari 2.0 support
+     *
+     * @author kangax
+     * @access private
+     * @function hasOwnProp
+     * @param {object} object - The object to check for a property
+     * @param {string} property - The property to check for
+     * @returns {boolean}
+     */
+
+    // hasOwnProperty shim by kangax needed for Safari 2.0 support
+    var hasOwnProp;
+
+    (function() {
+      var _hasOwnProperty = ({}).hasOwnProperty;
+      /* istanbul ignore else */
+      /* we have no way of testing IE 5.5 or safari 2,
+       * so just assume the else gets hit */
+      if (!is(_hasOwnProperty, 'undefined') && !is(_hasOwnProperty.call, 'undefined')) {
+        hasOwnProp = function(object, property) {
+          return _hasOwnProperty.call(object, property);
+        };
+      }
+      else {
+        hasOwnProp = function(object, property) { /* yes, this can give false positives/negatives, but most of the time we don't care about those */
+          return ((property in object) && is(object.constructor.prototype[property], 'undefined'));
+        };
+      }
+    })();
 
 
-    var createElement = function() {
+
+    /**
+     * A convenience helper to check if the document we are running in is an SVG document
+     *
+     * @access private
+     * @returns {boolean}
+     */
+
+    var isSVG = docElement.nodeName.toLowerCase() === 'svg';
+
+
+    /**
+     * createElement is a convenience wrapper around document.createElement. Since we
+     * use createElement all over the place, this allows for (slightly) smaller code
+     * as well as abstracting away issues with creating elements in contexts other than
+     * HTML documents (e.g. SVG documents).
+     *
+     * @access private
+     * @function createElement
+     * @returns {HTMLElement|SVGElement} An HTML or SVG element
+     */
+
+    function createElement() {
       if (typeof document.createElement !== 'function') {
         // This is the case in IE7, where the type of createElement is "object".
         // For this reason, we cannot call apply() as Object is not a Function.
         return document.createElement(arguments[0]);
+      } else if (isSVG) {
+        return document.createElementNS.call(document, 'http://www.w3.org/2000/svg', arguments[0]);
       } else {
         return document.createElement.apply(document, arguments);
       }
-    };
+    }
 
+    ;
     /*!
      {
      "name": "HTML5 Video",
@@ -242,27 +322,13 @@ function modernizerFactory() {
     });
 
 
-    // hasOwnProperty shim by kangax needed for Safari 2.0 support
-    var hasOwnProp;
-
-    (function() {
-      var _hasOwnProperty = ({}).hasOwnProperty;
-      /* istanbul ignore else */
-      /* we have no way of testing IE 5.5 or safari 2,
-       * so just assume the else gets hit */
-      if (!is(_hasOwnProperty, 'undefined') && !is(_hasOwnProperty.call, 'undefined')) {
-        hasOwnProp = function(object, property) {
-          return _hasOwnProperty.call(object, property);
-        };
-      }
-      else {
-        hasOwnProp = function(object, property) { /* yes, this can give false positives/negatives, but most of the time we don't care about those */
-          return ((property in object) && is(object.constructor.prototype[property], 'undefined'));
-        };
-      }
-    })();
-
-
+    /**
+     * setClasses takes an array of class names and adds them to the root element
+     *
+     * @access private
+     * @function setClasses
+     * @param {string[]} classes - Array of class names
+     */
 
     // Pass in an and array of class names, e.g.:
     //  ['no-webp', 'borderradius', ...]
@@ -270,8 +336,11 @@ function modernizerFactory() {
       var className = docElement.className;
       var classPrefix = Modernizr._config.classPrefix || '';
 
-      // Change `no-js` to `js` (we do this independently of the `enableClasses`
-      // option)
+      if (isSVG) {
+        className = className.baseVal;
+      }
+
+      // Change `no-js` to `js` (independently of the `enableClasses` option)
       // Handle classPrefix on this too
       if (Modernizr._config.enableJSClass) {
         var reJS = new RegExp('(^|\\s)' + classPrefix + 'no-js(\\s|$)');
@@ -281,46 +350,78 @@ function modernizerFactory() {
       if (Modernizr._config.enableClasses) {
         // Add the new classes
         className += ' ' + classPrefix + classes.join(' ' + classPrefix);
-        docElement.className = className;
+        isSVG ? docElement.className.baseVal = className : docElement.className = className;
       }
 
     }
 
     ;
 
-    // As far as I can think of, we shouldn't need or
-    // allow 'on' for non-async tests, and you can't do
-    // async tests without this 'addTest' module.
 
-    // Listeners for async or post-run tests
+    // _l tracks listeners for async tests, as well as tests that execute after the initial run
     ModernizrProto._l = {};
 
-    // 'addTest' implies a test after the core runloop,
-    // So we'll add in the events
-    ModernizrProto.on = function(test, cb) {
+    /**
+     * Modernizr.on is a way to listen for the completion of async tests. Being
+     * asynchronous, they may not finish before your scripts run. As a result you
+     * will get a possibly false negative `undefined` value.
+     *
+     * @memberof Modernizr
+     * @name Modernizr.on
+     * @access public
+     * @function on
+     * @param {string} feature - String name of the feature detect
+     * @param {function} cb - Callback function returning a Boolean - true if feature is supported, false if not
+     * @example
+     *
+     * ```js
+     * Modernizr.on('flash', function( result ) {
+   *   if (result) {
+   *    // the browser has flash
+   *   } else {
+   *     // the browser does not have flash
+   *   }
+   * });
+     * ```
+     */
+
+    ModernizrProto.on = function(feature, cb) {
       // Create the list of listeners if it doesn't exist
-      if (!this._l[test]) {
-        this._l[test] = [];
+      if (!this._l[feature]) {
+        this._l[feature] = [];
       }
 
       // Push this test on to the listener list
-      this._l[test].push(cb);
+      this._l[feature].push(cb);
 
       // If it's already been resolved, trigger it on next tick
-      if (Modernizr.hasOwnProperty(test)) {
+      if (Modernizr.hasOwnProperty(feature)) {
         // Next Tick
         setTimeout(function() {
-          Modernizr._trigger(test, Modernizr[test]);
+          Modernizr._trigger(feature, Modernizr[feature]);
         }, 0);
       }
     };
 
-    ModernizrProto._trigger = function(test, res) {
-      if (!this._l[test]) {
+    /**
+     * _trigger is the private function used to signal test completion and run any
+     * callbacks registered through [Modernizr.on](#modernizr-on)
+     *
+     * @memberof Modernizr
+     * @name Modernizr._trigger
+     * @access private
+     * @function _trigger
+     * @param {string} feature - string name of the feature detect
+     * @param {function|boolean} [res] - A feature detection function, or the boolean =
+     * result of a feature detection function
+     */
+
+    ModernizrProto._trigger = function(feature, res) {
+      if (!this._l[feature]) {
         return;
       }
 
-      var cbs = this._l[test];
+      var cbs = this._l[feature];
 
       // Force async
       setTimeout(function() {
@@ -332,18 +433,80 @@ function modernizerFactory() {
       }, 0);
 
       // Don't trigger these again
-      delete this._l[test];
+      delete this._l[feature];
     };
 
     /**
-     * addTest allows the user to define their own feature tests
-     * the result will be added onto the Modernizr object,
-     * as well as an appropriate className set on the html element
+     * addTest allows you to define your own feature detects that are not currently
+     * included in Modernizr (under the covers it's the exact same code Modernizr
+     * uses for its own [feature detections](https://github.com/Modernizr/Modernizr/tree/master/feature-detects)). Just like the offical detects, the result
+     * will be added onto the Modernizr object, as well as an appropriate className set on
+     * the html element when configured to do so
      *
-     * @param feature - String naming the feature
-     * @param test - Function returning true if feature is supported, false if not
+     * @memberof Modernizr
+     * @name Modernizr.addTest
+     * @optionName Modernizr.addTest()
+     * @optionProp addTest
+     * @access public
+     * @function addTest
+     * @param {string|object} feature - The string name of the feature detect, or an
+     * object of feature detect names and test
+     * @param {function|boolean} test - Function returning true if feature is supported,
+     * false if not. Otherwise a boolean representing the results of a feature detection
+     * @example
+     *
+     * The most common way of creating your own feature detects is by calling
+     * `Modernizr.addTest` with a string (preferably just lowercase, without any
+     * punctuation), and a function you want executed that will return a boolean result
+     *
+     * ```js
+     * Modernizr.addTest('itsTuesday', function() {
+   *  var d = new Date();
+   *  return d.getDay() === 2;
+   * });
+     * ```
+     *
+     * When the above is run, it will set Modernizr.itstuesday to `true` when it is tuesday,
+     * and to `false` every other day of the week. One thing to notice is that the names of
+     * feature detect functions are always lowercased when added to the Modernizr object. That
+     * means that `Modernizr.itsTuesday` will not exist, but `Modernizr.itstuesday` will.
+     *
+     *
+     *  Since we only look at the returned value from any feature detection function,
+     *  you do not need to actually use a function. For simple detections, just passing
+     *  in a statement that will return a boolean value works just fine.
+     *
+     * ```js
+     * Modernizr.addTest('hasJquery', 'jQuery' in window);
+     * ```
+     *
+     * Just like before, when the above runs `Modernizr.hasjquery` will be true if
+     * jQuery has been included on the page. Not using a function saves a small amount
+     * of overhead for the browser, as well as making your code much more readable.
+     *
+     * Finally, you also have the ability to pass in an object of feature names and
+     * their tests. This is handy if you want to add multiple detections in one go.
+     * The keys should always be a string, and the value can be either a boolean or
+     * function that returns a boolean.
+     *
+     * ```js
+     * var detects = {
+   *  'hasjquery': 'jQuery' in window,
+   *  'itstuesday': function() {
+   *    var d = new Date();
+   *    return d.getDay() === 2;
+   *  }
+   * }
+     *
+     * Modernizr.addTest(detects);
+     * ```
+     *
+     * There is really no difference between the first methods and this one, it is
+     * just a convenience to let you write more readable code.
      */
+
     function addTest(feature, test) {
+
       if (typeof feature == 'object') {
         for (var key in feature) {
           if (hasOwnProp(feature, key)) {
@@ -397,11 +560,11 @@ function modernizerFactory() {
       return Modernizr; // allow chaining.
     }
 
-    // After all the tests are run, add self
-    // to the Modernizr prototype
+    // After all the tests are run, add self to the Modernizr prototype
     Modernizr._q.push(function() {
       ModernizrProto.addTest = addTest;
     });
+
 
 
     /*!
@@ -424,12 +587,13 @@ function modernizerFactory() {
       var waitTime = 300;
       var elem = createElement('video');
       var elemStyle = elem.style;
-      var testAutoplay = function(arg) {
+
+      function testAutoplay(arg) {
         clearTimeout(timeout);
-        elem.removeEventListener('playing', testAutoplay);
+        elem.removeEventListener('playing', testAutoplay, false);
         addTest('videoautoplay', arg && arg.type === 'playing' || elem.currentTime !== 0);
         elem.parentNode.removeChild(elem);
-      };
+      }
 
       //skip the test if video itself, or the autoplay
       //element on it isn't supported
@@ -461,12 +625,12 @@ function modernizerFactory() {
       }
 
       elem.setAttribute('autoplay', '');
-      elem.style = 'display:none';
+      elem.style.cssText = 'display:none';
       docElement.appendChild(elem);
       // wait for the next tick to add the listener, otherwise the element may
       // not have time to play in high load situations (e.g. the test suite)
       setTimeout(function() {
-        elem.addEventListener('playing', testAutoplay);
+        elem.addEventListener('playing', testAutoplay, false);
         timeout = setTimeout(testAutoplay, waitTime);
       }, 0);
     });
