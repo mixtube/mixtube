5b6815a15cb8dca2a34193f2b764e3f109751770
Jonathan Raoult <jesuisjonathan@gmx.fr>
2017-02-04 18:49:07 +0000

    - replaced svgmin by svgstore to benefit from the defs extraction from symbol (cf https://github.com/w0rm/gulp-svgstore/issues/31 and https://github.com/w0rm/gulp-svgstore/pull/32)

--- build/src/buildSvg.js
+++ build/src/buildSvg.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const gulp = require('gulp'),
-  svgSprite = require('gulp-svg-sprite'),
+  svgmin = require('gulp-svgmin'),
+  svgstore = require('gulp-svgstore'),
+  rename = require('gulp-rename'),
   path = require('path');
 
 /**
@@ -11,38 +13,6 @@ const gulp = require('gulp'),
 module.exports = function makeBuildSvg(config) {
 
   const resolvedLogoPath = path.resolve(config.svgLogoPath);
-  const svgSpriteConf = {
-    svg: {
-      xmlDeclaration: false,
-      doctypeDeclaration: false
-    },
-    shape: {
-      id: {
-        // always set the id of the provided logo to "m-logo"
-        generator(name, vinyl) {
-          return vinyl.path === resolvedLogoPath ? 'm-logo' : vinyl.stem;
-        }
-      }
-    },
-    // make sure the svgo phase is not breaking the SVG (removeUnknownsAndDefaults breaks the logo)
-    transform: [{
-      svgo: {
-        js2svg: {
-          pretty: !config.production
-        },
-        plugins: [{
-          removeStyleElement: true,
-          removeUnknownsAndDefaults: false
-        }]
-      }
-    }],
-    mode: {
-      symbol: {
-        dest: '.',
-        sprite: 'sprite.svg'
-      }
-    }
-  };
 
   const svgPaths = [
     'node_modules/Ionicons/src/ios-search.svg',
@@ -64,8 +34,30 @@ module.exports = function makeBuildSvg(config) {
     return runSvgPipeline();
 
     function runSvgPipeline() {
-      return gulp.src(svgPaths)
-        .pipe(svgSprite(svgSpriteConf))
+      return gulp.src(svgPaths, {
+        // forces gulp to set a base (any base would work here)
+        // it is just to get dirname in gulp rename so that we can resolve the absolute logo path
+        base: '.'
+      })
+        .pipe(rename(file => {
+          // counter intuitive but by renaming the file we change the id of the resulting sprite
+          if (path.resolve(file.dirname, file.basename + file.extname) === resolvedLogoPath) {
+            file.basename = 'mt-logo';
+          }
+        }))
+        .pipe(svgmin({
+            js2svg: {
+              pretty: !config.production
+            },
+            plugins: [{
+              removeStyleElement: true,
+            }, {
+              removeUnknownsAndDefaults: false
+            }]
+          }
+        ))
+        .pipe(svgstore())
+        .pipe(rename('sprite.svg'))
         .pipe(gulp.dest(config.publicDirPath));
     }
   };
