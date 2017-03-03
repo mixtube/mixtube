458bd13988b65059e8cc3ff87ffbb7e81aab27c0
Jonathan Raoult <jesuisjonathan@gmx.fr>
2017-03-03 23:40:44 +0000

    added a function based on express

--- server/httpYoutubeGetVideoInfo.js
+++ server/httpYoutubeGetVideoInfo.js
@@ -1,30 +1,9 @@
 'use strict';
 
 const url = require('url');
-const express = require('express');
-const compression = require('compression');
 const youtubeGetVideoInfo = require('./functions/youtubeGetVideoInfo');
 
-const PORT = process.env.PORT || 8080;
-const app = express();
-
-if (process.env.NODE_ENV !== 'development') {
-  app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500);
-  });
-}
-
-app.use(compression());
-
-// enable CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
-app.get('/v1/youtube/extra/videos', (req, res, next) => {
+exports.httpYoutubeGetVideoInfo = (req, res, next) => {
   const { id, origin } = req.query;
   if (!id) {
     next(new Error('Missing id parameter'));
@@ -49,12 +28,4 @@ app.get('/v1/youtube/extra/videos', (req, res, next) => {
   youtubeGetVideoInfo(videoIds, origin)
     .then(infoList => res.json(infoList.map(transformInfo)))
     .catch(next);
-});
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
-
-function transformInfo({ id, status, errorcode }) {
-  return { id, blacklisted: status === 'fail' && errorcode === '150' };
-}
+};
